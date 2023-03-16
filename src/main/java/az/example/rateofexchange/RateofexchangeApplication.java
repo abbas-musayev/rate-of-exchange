d5d2a1be32e9d8c07f9e10a944a993a145fc7949
abbas_musayev <abbas99musayev@gmail.com>
2023-03-16 21:56:33 +0400

    initial commit

---
+++ src/main/java/az/example/rateofexchange/RateofexchangeApplication.java
@@ -0,0 +1,13 @@
+package az.example.rateofexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateofexchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RateofexchangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/az/example/rateofexchange/RateofexchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package az.example.rateofexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateofexchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
